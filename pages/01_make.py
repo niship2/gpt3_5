693fc372bdcf57cc660b4412963286e1ace34d9c
niship2 <k-nishio@enlighton.co.jp>
2023-03-02 22:45:25 +0900

    fix:fix indent

--- pages/01_make.py
+++ pages/01_make.py
@@ -40,7 +40,7 @@ def get_compilation(txt, title="", abst="", claims="", desc="", input_type="titl
                     {"role": "user", "content": "特許文章らしい特許請求の範囲を作成してください。【請求項１】という見出しをつけて下さい。文章は「～と、」「～と、」「～を備え、」という形で構成を列挙した上で、最後に「～を特徴とする～」という１文章で作成して下さい。"},
                 ]
             )
-        return response.choices[0].message.content
+            return response.choices[0].message.content
     except Exception as e:
         return "error!" + st.write(e)
 
@@ -49,8 +49,7 @@ if check_password():
     #st.set_page_config(page_title="明細書作成ページ", page_icon="🌍", layout="wide")
     pd.options.display.precision = 1
 
-    txt = st.text_area('アイデアを入力してください。', '''
-        【課題】半導体装置の薄型化と剥離強度の維持をした平板形状の半導体装置を提供する。
+    txt = st.text_area('アイデアを入力してください。', '''課題】半導体装置の薄型化と剥離強度の維持をした平板形状の半導体装置を提供する。
         【解決手段】平板形状のカソード電極１の一端に底部が平坦な椀形状部１２を形成し、前記椀形状部１２の内部にダイオード素子３をはんだ付けし、前記ダイオード素子３の上面に、平板形状のアノード電極２の一端２１をはんだ付けし、前記椀形状部の内部を絶縁樹脂で充填した。
         ''')
 
