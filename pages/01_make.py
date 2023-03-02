0148bfc68e0ebde63bc9d7abe0dee0748c0b0937
niship2 <k-nishio@enlighton.co.jp>
2023-03-02 22:28:16 +0900

    modify: add abst

--- pages/01_make.py
+++ pages/01_make.py
@@ -16,6 +16,16 @@ def get_compilation(txt, title="", abst="", claims="", desc="", input_type="titl
                 {"role": "user", "content": "以下の文章に特許文章らしいタイトルを付けてください。" + txt},
             ]
         )
+    if input_type == "abst":
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "あなたは特許文章を作成する人です。"},
+                {"role": "user", "content": "以下の文章に特許文章らしいタイトルを付けてください。" + txt},
+                {"role": "system", "content": title},
+                {"role": "user", "content": "特許文章らしい要約を作成してください。【課題】と【解決手段】という見出しを加えてください。であるという語尾で作成して下さい。"},
+            ]
+        )
 
     return response.choices[0].message.content
 
@@ -24,7 +34,7 @@ if check_password():
     #st.set_page_config(page_title="明細書作成ページ", page_icon="🌍", layout="wide")
     pd.options.display.precision = 1
 
-    txt = st.text_area('Text to analyze', '''
+    txt = st.text_area('アイデアを入力してください。', '''
         【課題】半導体装置の薄型化と剥離強度の維持をした平板形状の半導体装置を提供する。
         【解決手段】平板形状のカソード電極１の一端に底部が平坦な椀形状部１２を形成し、前記椀形状部１２の内部にダイオード素子３をはんだ付けし、前記ダイオード素子３の上面
             に、平板形状のアノード電極２の一端２１をはんだ付けし、前記椀形状部の内部を絶縁樹脂で充填した。
@@ -34,7 +44,11 @@ if check_password():
                             claims="", desc="", input_type="title")
     st.write(title)
 
-    abst = ""
+    abst = get_compilation(txt, title=title, abst="",
+                           claims="", desc="", input_type="abst")
+
+    st.write(abst)
+
     claims = ""
     desc = ""
 
