976e458de1e4fb83e056150966b24da4ded9262a
niship2 <k-nishio@enlighton.co.jp>
2023-03-02 22:44:05 +0900

    fix add error message

--- pages/01_make.py
+++ pages/01_make.py
@@ -41,8 +41,8 @@ def get_compilation(txt, title="", abst="", claims="", desc="", input_type="titl
                 ]
             )
         return response.choices[0].message.content
-    except:
-        return "error!"
+    except Exception as e:
+        return "error!" + st.write(e)
 
 
 if check_password():
@@ -51,8 +51,7 @@ if check_password():
 
     txt = st.text_area('アイデアを入力してください。', '''
         【課題】半導体装置の薄型化と剥離強度の維持をした平板形状の半導体装置を提供する。
-        【解決手段】平板形状のカソード電極１の一端に底部が平坦な椀形状部１２を形成し、前記椀形状部１２の内部にダイオード素子３をはんだ付けし、前記ダイオード素子３の上面
-            に、平板形状のアノード電極２の一端２１をはんだ付けし、前記椀形状部の内部を絶縁樹脂で充填した。
+        【解決手段】平板形状のカソード電極１の一端に底部が平坦な椀形状部１２を形成し、前記椀形状部１２の内部にダイオード素子３をはんだ付けし、前記ダイオード素子３の上面に、平板形状のアノード電極２の一端２１をはんだ付けし、前記椀形状部の内部を絶縁樹脂で充填した。
         ''')
 
     title = get_compilation(txt, title="", abst="",
