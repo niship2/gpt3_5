a4d61a36c809ebb6ef1f492fe8377eceff8e2f91
niship2 <k-nishio@enlighton.co.jp>
2023-03-03 11:51:41 +0900

    \r\n の位置修正

--- pages/02_出力.py
+++ pages/02_出力.py
@@ -65,8 +65,8 @@ if check_password():
         desc_out = ""
 
         for i in range(0, len(desc_temp_list)-1):
-            desc_out += "【" + str(i+1).zfill(4) + "】" + \
-                desc_temp_list[i] + "。\r\n"
+            desc_out += "\r\n【" + str(i+1).zfill(4) + "】" + \
+                desc_temp_list[i] + "。"
         st.write(desc_out)
     with tab5:
         st.header("図面")
