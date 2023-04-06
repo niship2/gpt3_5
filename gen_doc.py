75ef54d49a10cb95fbfd53304da4389531352ed0
niship2 <k-nishio@enlighton.co.jp>
2023-04-06 18:35:42 +0900

    add gpt4

--- gen_doc.py
+++ gen_doc.py
@@ -5,5 +5,24 @@ import numpy as np
 from streamlit_app import check_password
 
 
-def get_entry():
-    pass
+# Note: The openai-python library support for Azure OpenAI is in preview.
+import os
+import openai
+openai.api_type = "azure"
+openai.api_base = st.secrets["OPENAI_API_BASE"]
+openai.api_version = "2023-03-15-preview"
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+response = openai.ChatCompletion.create(
+    engine="gpt-4",  # engine = "deployment_name".
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Does Azure OpenAI support customer managed keys?"},
+        {"role": "assistant",
+            "content": "Yes, customer managed keys are supported by Azure OpenAI."},
+        {"role": "user", "content": "Do other Azure Cognitive Services support this too?"}
+    ]
+)
+
+print(response)
+print(response['choices'][0]['message']['content'])
