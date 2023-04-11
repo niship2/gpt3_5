882c8f6db1217097dcddebd9b28d870a5394583a
niship2 <48500095+niship2@users.noreply.github.com>
2023-04-11 01:12:28 +0000

    modify:願書編集可能に

--- pages/01_文章作成.py
+++ pages/01_文章作成.py
@@ -3,7 +3,12 @@ import pandas as pd
 import numpy as np
 from streamlit_app import check_password
 import openai
-from gen_sentence import get_completion_title, get_completion_abst, get_completion_claims, get_completion_desc
+from gen_sentence import (
+    get_completion_title,
+    get_completion_abst,
+    get_completion_claims,
+    get_completion_desc,
+)
 
 openai.api_key = st.secrets["API-KEY"]
 openai.api_type = "azure"
@@ -13,56 +18,54 @@ openai.api_version = "2023-03-15-preview"
 
 st.set_page_config(page_title="明細書作成ページ", page_icon="🌍", layout="wide")
 
-option = st.sidebar.selectbox(
-    '生成モデル選択',
-    ('gpt-3.5', 'gpt-4'))
+option = st.sidebar.selectbox("生成モデル選択", ("gpt-3.5", "gpt-4"))
 
 # 初期値
 try:
     txt = st.session_state.txt
 except:
     txt = "半導体装置の薄型化と剥離強度の維持をした平板形状の半導体装置を提供する。平板形状のカソード電極の一端に底部が平坦な椀形状部を形成し、前記椀形状部の内部にダイオード素子をはんだ付けし、前記ダイオード素子の上面に、平板形状のアノード電極の一端をはんだ付けし、前記椀形状部の内部を絶縁樹脂で充填した。"
-    st.session_state['txt'] = txt
+    st.session_state["txt"] = txt
 
 try:
     title = st.session_state.title
 except:
     title = ""
-    st.session_state['title'] = title
+    st.session_state["title"] = title
 try:
     abst = st.session_state.abst
 except:
     abst = ""
-    st.session_state['abst'] = abst
+    st.session_state["abst"] = abst
 try:
     claims = st.session_state.claims
 except:
     claims = ""
-    st.session_state['claims'] = claims
+    st.session_state["claims"] = claims
 
 try:
     instruction_claims_text = st.session_state.instruction_claims_text
 except:
     instruction_claims_text = ""
-    st.session_state['instruction_claims_text'] = instruction_claims_text
+    st.session_state["instruction_claims_text"] = instruction_claims_text
 
 try:
     desc = st.session_state.desc
 except:
     desc = ""
-    st.session_state['desc'] = desc
+    st.session_state["desc"] = desc
 
 try:
     instruction_desc_text = st.session_state.instruction_desc_text
 except:
     instruction_desc_text = ""
-    st.session_state['instruction_desc_text'] = instruction_desc_text
+    st.session_state["instruction_desc_text"] = instruction_desc_text
 
 
 def merge_instruction(opts, tex):
     # 初期値で空要素が入ってしまうので削除
     tex_list = []
-    tex_list = [x for x in tex.replace("\n", "").split("。") if x != '']
+    tex_list = [x for x in tex.replace("\n", "").split("。") if x != ""]
     for opt in opts:
         if opt in tex_list:
             pass
@@ -74,8 +77,7 @@ def merge_instruction(opts, tex):
 if check_password():
     pd.options.display.precision = 1
 
-    txt = st.text_area(
-        'アイデアを入力してください。', txt)
+    txt = st.text_area("アイデアを入力してください。", txt)
 
     st.markdown("---")
 
@@ -84,13 +86,15 @@ if check_password():
     with st.container():
         with title_inst_col:
             instruction_title = st.text_area(
-                '文章生成指示文を入力してください', value="10文字程度で発明の名称を作成してください。", placeholder="特許文章らしいタイトルを１０文字以内で作成してください。")
+                "文章生成指示文を入力してください",
+                value="10文字程度で発明の名称を作成してください。",
+                placeholder="特許文章らしいタイトルを１０文字以内で作成してください。",
+            )
             if st.button("名称作成！"):
-                title = get_completion_title(
-                    txt, instruction_title=instruction_title)
+                title = get_completion_title(txt, instruction_title=instruction_title)
                 st.info("アイデア部分の文章と文章生成指示文をもとに作成しました。")
             else:
-                title = st.session_state['title']
+                title = st.session_state["title"]
         with title_gen_col:
             st.text_area("発明の名称", value=title)
 
@@ -101,13 +105,21 @@ if check_password():
     with st.container():
         with abst_inst_col:
             instruction_abst = st.text_area(
-                '要約生成指示文を入力してください', value="特許文章らしい要約として修正してください。【課題】と【解決手段】という見出しを加えてください。であるという語尾で作成して下さい。", placeholder="特許文章らしい要約を作成してください。【課題】と【解決手段】という見出しを加えてください。であるという語尾で作成して下さい。")
+                "要約生成指示文を入力してください",
+                value="特許文章らしい要約として修正してください。【課題】と【解決手段】という見出しを加えてください。であるという語尾で作成して下さい。",
+                placeholder="特許文章らしい要約を作成してください。【課題】と【解決手段】という見出しを加えてください。であるという語尾で作成して下さい。",
+            )
             if st.button("要約修正！"):
                 abst = get_completion_abst(
-                    txt, title=title, instruction_title=instruction_title, instruction_abst=instruction_abst, abst=abst)
+                    txt,
+                    title=title,
+                    instruction_title=instruction_title,
+                    instruction_abst=instruction_abst,
+                    abst=abst,
+                )
                 st.info("アイデア、タイトル、要約生成指示文をもとに作成しました。")
             else:
-                abst = st.session_state['abst']
+                abst = st.session_state["abst"]
         with abst_gen_col:
             st.text_area("要約", value=abst, height=200)
 
@@ -118,26 +130,42 @@ if check_password():
     with st.container():
         with claims_inst_col:
             instruction_claim_opt = st.multiselect(
-                '指示文例選択',
-                default=['特許文章らしい特許請求の範囲で記載してください'],
-                options=('特許文章らしい特許請求の範囲で記載してください',
-                         '各特許請求の範囲は１文で作成して下さい',
-                         '【請求項１】という見出しを加えて下さい',
-                         '【請求項2】を追加して、請求項2には請求項１の構成のどれかを具体化したものとして生成してください')
-
+                "指示文例選択",
+                default=["特許文章らしい特許請求の範囲で記載してください"],
+                options=(
+                    "特許文章らしい特許請求の範囲で記載してください",
+                    "各特許請求の範囲は１文で作成して下さい",
+                    "【請求項１】という見出しを加えて下さい",
+                    "【請求項2】を追加して、請求項2には請求項１の構成のどれかを具体化したものとして生成してください",
+                ),
             )
-            instruction_claims_text = "。\n".join(merge_instruction(
-                instruction_claim_opt, instruction_claims_text)) + "。"  # 最後の分に。を付加
+            instruction_claims_text = (
+                "。\n".join(
+                    merge_instruction(instruction_claim_opt, instruction_claims_text)
+                )
+                + "。"
+            )  # 最後の分に。を付加
             instruction_claims = st.text_area(
-                '請求項生成指示文を作成してください', value=instruction_claims_text, placeholder="", height=250)
+                "請求項生成指示文を直接入力or選択で作成してください",
+                value=instruction_claims_text,
+                placeholder="",
+                height=250,
+            )
 
             if st.button("請求項修正！"):
                 claims = get_completion_claims(
-                    txt, title=title, abst=abst, instruction_title=instruction_title, instruction_abst=instruction_abst, instruction_claims=instruction_claims, claims=claims)
+                    txt,
+                    title=title,
+                    abst=abst,
+                    instruction_title=instruction_title,
+                    instruction_abst=instruction_abst,
+                    instruction_claims=instruction_claims,
+                    claims=claims,
+                )
                 st.info("アイデア、タイトル、要約、請求項生成指示文をもとに作成しました。")
 
             else:
-                claims = st.session_state['claims']
+                claims = st.session_state["claims"]
 
         with claims_gen_col:
             st.text_area("請求項", value=claims, height=400)
@@ -149,30 +177,46 @@ if check_password():
     with st.container():
         with desc_inst_col:
             instruction_desc_opt = st.multiselect(
-                '指示文例選択',
-                default=['特許文章らしい明細書の文章として修正してください'],
-                options=('特許文章らしい明細書の文章として修正してください',
-                         '文章毎に【0001】のように見出しを追加してください',
-                         '各見出しで改行して下さい',
-                         '文章は「である」「であった」などの語尾で作成して下さい'
-                         '【発明の詳細な説明】、【技術分野】、【背景技術】、【先行技術文献】、【発明が解決しようとする課題】、【課題を解決するための手段】、【図面の簡単な説明】、【発明を実施するための形態】という見出しをこの順番で加えてください',
-                         '【背景技術】の部分では先行技術の欠点を説明してください',
-                         '【先行技術文献】では先行技術文献の番号を入れてください',
-                         '【課題を解決するための手段】には生成した特許請求の範囲の文章を記載してください。'
-                         '【発明の詳細な説明】は５段落作成して請求項の各構成部品について具体例を記載してください。'
-                         )
-
+                "指示文例選択",
+                default=["特許文章らしい明細書の文章として修正してください"],
+                options=(
+                    "特許文章らしい明細書の文章として修正してください",
+                    "文章毎に【0001】のように見出しを追加してください",
+                    "各見出しで改行して下さい",
+                    "文章は「である」「であった」などの語尾で作成して下さい"
+                    "【発明の詳細な説明】、【技術分野】、【背景技術】、【先行技術文献】、【発明が解決しようとする課題】、【課題を解決するための手段】、【図面の簡単な説明】、【発明を実施するための形態】という見出しをこの順番で加えてください",
+                    "【背景技術】の部分では先行技術の欠点を説明してください",
+                    "【先行技術文献】では先行技術文献の番号を入れてください",
+                    "【課題を解決するための手段】には生成した特許請求の範囲の文章を記載してください。"
+                    "【発明の詳細な説明】は５段落作成して請求項の各構成部品について具体例を記載してください。",
+                ),
             )
-            instruction_desc_text = "。\n".join(merge_instruction(
-                instruction_desc_opt, instruction_desc_text)) + "。"  # 最後の分に。を付加
+            instruction_desc_text = (
+                "。\n".join(
+                    merge_instruction(instruction_desc_opt, instruction_desc_text)
+                )
+                + "。"
+            )  # 最後の分に。を付加
             instruction_desc = st.text_area(
-                '明細書生成指示文を入力してください', value=instruction_desc_text, placeholder="", height=250)
+                "明細書生成指示文を直接入力or選択で入力してください",
+                value=instruction_desc_text,
+                placeholder="",
+                height=250,
+            )
             if st.button("明細書修正！"):
-                desc = get_completion_desc(txt, title=title, abst=abst, instruction_title=instruction_title, instruction_abst=instruction_abst,
-                                           instruction_claims=instruction_claims, claims=claims, instruction_desc=instruction_desc)
+                desc = get_completion_desc(
+                    txt,
+                    title=title,
+                    abst=abst,
+                    instruction_title=instruction_title,
+                    instruction_abst=instruction_abst,
+                    instruction_claims=instruction_claims,
+                    claims=claims,
+                    instruction_desc=instruction_desc,
+                )
                 st.info("アイデア、タイトル、要約、請求項、明細書生成指示文をもとに作成しました。")
             else:
-                desc = st.session_state['desc']
+                desc = st.session_state["desc"]
         with desc_gen_col:
             st.text_area("明細書", value=desc, height=400)
 
@@ -182,7 +226,8 @@ if check_password():
     with st.container():
         with img_inst_col:
             instruction_img = st.text_area(
-                '図面生成指示文を入力してください', value=title, placeholder="工事中")
+                "図面生成指示文を入力してください", value=title, placeholder="工事中"
+            )
 
             if st.button("図面生成"):
                 pass
@@ -200,15 +245,15 @@ if check_password():
             # except:
             #    pass
 
-    st.session_state['txt'] = txt
-    st.session_state['title'] = title
-    st.session_state['instruction_title'] = instruction_title
-    st.session_state['abst'] = abst
-    st.session_state['claims'] = claims
-    st.session_state['desc'] = desc
-    st.session_state['option'] = option
-    st.session_state['instruction_claims_text'] = instruction_claims_text
-    st.session_state['instruction_desc_text'] = instruction_desc_text
+    st.session_state["txt"] = txt
+    st.session_state["title"] = title
+    st.session_state["instruction_title"] = instruction_title
+    st.session_state["abst"] = abst
+    st.session_state["claims"] = claims
+    st.session_state["desc"] = desc
+    st.session_state["option"] = option
+    st.session_state["instruction_claims_text"] = instruction_claims_text
+    st.session_state["instruction_desc_text"] = instruction_desc_text
 
     st.experimental_set_query_params(
         # comp1=comp1,
--- pages/02_出力.py
+++ pages/02_出力.py
@@ -2,14 +2,24 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 from streamlit_app import check_password
+import time
 
 if check_password():
     pd.options.display.precision = 1
 
+    with st.sidebar.container():
+        if st.sidebar.button("提出！", use_container_width=True):
+            placeholder = st.empty()
+            with placeholder.container():
+                time.sleep(2)
+                st.info("提出できませんでした", icon="ℹ️")
+            time.sleep(3)
+            placeholder.empty()
+
     # st.set_page_config(page_title="Report", page_icon="🌍", layout="wide")
 
-    def file_downloader(filename, file_label='File'):
-        with open(filename, 'rb') as f:
+    def file_downloader(filename, file_label="File"):
+        with open(filename, "rb") as f:
             data = f.read()
 
     # time_stamp = st.session_state.timestamp
@@ -19,30 +29,44 @@ if check_password():
     claims = st.session_state.claims
     desc = st.session_state.desc
 
+    try:
+        bibtable = st.session_state.bibtable
+    except:
+        bibtable = pd.DataFrame()
+        st.session_state["bibtable"] = bibtable
+
     tab1, tab2, tab3, tab4, tab5 = st.tabs(["願書", "要約", "請求項", "明細書", "図面"])
 
     with tab1:
-        st.header("願書")
-        st.write("【書類名】 特許願")
-        st.write("【整理番号】")
-        st.write("【提出日】 令和 年 月 日")
-        st.write("【あて先】 特許庁長官 殿")
-        st.write("【国際特許分類】")
-        st.write("【発明者】")
-        st.write("【住所又は居所】")
-        st.write("【氏名】")
-        st.write("【特許出願人】")
-        st.write("【識別番号】）")
-        st.write("【住所又は居所】")
-        st.write("【氏名又は名称】")
-        st.write("（【代表者】）")
-        st.write("（【国籍・地域】）")
-        st.write("（【電話番号】）")
-        st.write("【提出物件の目録】")
-        st.write("【物件名】 特許請求の範囲 １")
-        st.write("【物件名】 明細書 １")
-        st.write("（【物件名】 図面 １）")
-        st.write("【物件名】 要約書 １")
+        df = pd.DataFrame(
+            [
+                {"項目": "願書", "値": ""},
+                {"項目": "【書類名】", "値": "特許願"},
+                {"項目": "【整理番号】", "値": "***"},
+                {"項目": "【提出日】", "値": "令和 年 月 日"},
+                {"項目": "【あて先】", "値": "特許庁長官 殿"},
+                {"項目": "【国際特許分類】", "値": "***"},
+                {"項目": "【発明者】", "値": "***"},
+                {"項目": "【住所又は居所】", "値": "***"},
+                {"項目": "【氏名】", "値": "***"},
+                {"項目": "【特許出願人】", "値": "***"},
+                {"項目": "【識別番号】", "値": "***"},
+                {"項目": "【住所又は居所】", "値": "***"},
+                {"項目": "【氏名又は名称】", "値": "***"},
+                {"項目": "【提出物件の目録】", "値": ""},
+                {"項目": "【物件名】 特許請求の範囲", "値": "1"},
+                {"項目": "【物件名】 明細書", "値": "1"},
+                {"項目": "【物件名】 図面", "値": "1"},
+                {"項目": "【物件名】 要約書", "値": "1"},
+            ]
+        )
+        with st.expander("願書編集"):
+            st.write("セルをクリックして編集できます。")
+            st.write("エクセル等からコピペできます。")
+            bibtable = st.experimental_data_editor(df, width=400, num_rows="dynamic")
+
+        for index, row in bibtable.iterrows():
+            st.write(row["項目"] + str(row["値"]))
 
     with tab2:
         st.header("要約")
@@ -64,7 +88,7 @@ if check_password():
 
         desc_out = ""
 
-        for i in range(0, len(desc_temp_list)-1):
+        for i in range(0, len(desc_temp_list) - 1):
             desc_out += desc_temp_list[i]
         st.write(desc_out)
     with tab5:
